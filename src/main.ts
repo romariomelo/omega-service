8edc5ea308ff1244c626ace7de319970c18762ad
romario <romario.melos@gmail.com>
2021-08-17 14:40:57 -0300

    Configuração para Heroku

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+const PORT = process.env.PORT;
+
+console.log(PORT);
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(3000);
+  await app.listen(PORT || 3000);
 }
 bootstrap();
