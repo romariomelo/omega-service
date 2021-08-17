1693230f80d6b97727396e7943b3783ca494a468
BrunaSepulveda <sepulvedabruna@gmail.com>
2021-08-17 15:08:56 -0300

    tentativa de criar um pullrequest

--- src/controllers/carga.controllers.ts
+++ src/controllers/carga.controllers.ts
@@ -3,6 +3,7 @@ import { Carga } from '../entities/carga.entity';
 
 @Controller('carga')
 export class CargaController {
+  // constructor(private readonly usuarioService: UsuarioService) {}
   @Get()
   async findAll(): Promise<Carga[]> {
     const propostas = await this.cargaService.findAll();
